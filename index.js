1033770535131fd84ec5e39327af959c376dada1
Melissa Faulner <melissa.faulner@gmail.com>
2021-02-10 16:23:50 -0500

    commit tree c566936ec9dee8600fce6c7339233dca5b01e13a

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
-// Write your code here
+// DELIVERABLE 1
+let num1 = 2
+let num2 = 31   
+let multiply = num1 * num2
+
+// DELIVERABLE 2
+
+let random = Math.ceil(Math.random() * 100) + 1
+
+// DELIVERABLE 3
+let num3 = 64
+let num4 = 5
+
+let mod = num3 % num4
+
+// DELIVERABLE 4
+let max = Math.max(12, 3, 7, 20)
